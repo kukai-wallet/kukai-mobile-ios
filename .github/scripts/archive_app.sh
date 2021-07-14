caf9cb5b4c890c0ceb1fa4e352da35a2c6f180a3
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-07-14 14:46:40 +0100

    - update deploy script using github example - remove old unnecessary files

--- .github/scripts/archive_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -scheme "Kukai Mobile Release"\
-            -sdk iphoneos \
-            -configuration release \
-            -archivePath $PWD/build/Kukai-mobile.xcarchive \
-            clean archive
--- .github/scripts/export_ipa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -archivePath $PWD/build/Kukai-mobile.xcarchive \
-            -exportOptionsPlist Kukai\ Mobile/ExportOptions.plist \
-            -exportPath $PWD/build \
-            -allowProvisioningUpdates \
-            -exportArchive
