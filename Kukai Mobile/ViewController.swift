b1e259b52d2b30cc0f771ca43a5351175186473c
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-07-14 11:20:00 +0100

    - initial project commit - setup app id's, schemes and icons for debug, release and beta

---
+++ Kukai Mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kukai Mobile
+//
+//  Created by Simon Mcloughlin on 14/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Kukai MobileTests/Kukai_MobileTests.swift
@@ -0,0 +1,33 @@
+//
+//  Kukai_MobileTests.swift
+//  Kukai MobileTests
+//
+//  Created by Simon Mcloughlin on 14/07/2021.
+//
+
+import XCTest
+@testable import Kukai_Mobile
+
+class Kukai_MobileTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
