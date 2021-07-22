346ea520b86bfd42a6858cd1ad9c30d9ab045c2c
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-07-22 13:04:11 +0100

    - add Sentry for crash reporting - setup Sentry to strip out any unique identifiers - update Beta build script to upload dSYM's to Sentry

--- Kukai Mobile/AppDelegate.swift
+++ Kukai Mobile/AppDelegate.swift
@@ -6,13 +6,35 @@
 //
 
 import UIKit
+import Sentry
+import os.log
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		
+		// Global appearenace proxy styles
 		setAppearenceProxies()
+		
+		
+		// Setup Sentry, but with Anonymous events
+		do {
+			let sentryOptions = try Options(dict: ["dsn": "https://53b9190ed8364ecc9b418dcc3493c506@o926227.ingest.sentry.io/5875459"])
+			sentryOptions.beforeSend = { (event) -> Event? in
+				
+				// Scrub any identifiable data to keep users anonymous
+				event.context?["app"]?.removeValue(forKey: "device_app_hash")
+				event.user = nil
+				
+				return event
+			}
+			
+			SentrySDK.start(options: sentryOptions)
+			
+		} catch (let error) {
+			os_log(.error, log: .default, "Sentry throw an error: %@", "\(error)")
+		}
 		
 		return true
 	}
