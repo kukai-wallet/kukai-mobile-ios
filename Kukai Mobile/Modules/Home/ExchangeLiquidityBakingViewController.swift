35ea5dcebfb94571f84f74cd81a3ba6fd9281595
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-07-30 12:34:20 +0100

    updated UI to add Token to XTZ as well as XTZ to token

--- Kukai Mobile/Modules/Home/ExchangeLiquidityBakingViewController.swift
+++ Kukai Mobile/Modules/Home/ExchangeLiquidityBakingViewController.swift
@@ -14,8 +14,11 @@ class ExchangeLiquidityBakingViewController: UIViewController {
 	@IBOutlet weak var outputTextField: UITextField!
 	@IBOutlet weak var swapButton: UIButton!
 	
+	@IBInspectable var isXtzToToken: Bool = true
+	
 	private var poolData: (xtzPool: XTZAmount, tokenPool: TokenAmount)? = nil
-	private var xtz: XTZAmount? = nil
+	private var xtzToSwap: XTZAmount? = nil
+	private var tokenToSwap: TokenAmount? = nil
 	private var calculationResult: DexterCalculationResult? = nil
 	
 	override func viewDidLoad() {
@@ -38,21 +41,61 @@ class ExchangeLiquidityBakingViewController: UIViewController {
 	}
 	
 	@IBAction func swapButton(_ sender: Any) {
-		guard let calc = calculationResult, calc.minimum > TokenAmount.zero(), let xtzToSwap = xtz, let wallet = DependencyManager.shared.selectedWallet else {
+		guard let calc = calculationResult, calc.minimum > TokenAmount.zero(), let wallet = DependencyManager.shared.selectedWallet else {
 			return
 		}
 		
-		let operations = OperationFactory.liquidityBakingXtzToToken(xtzAmount: xtzToSwap, minTokenAmount: calc.minimum, contract: "KT1TxqZ8QtKvLu3V3JH7Gx58n7Co8pgtpQU5", wallet: wallet, timeout: 60 * 5)
-		DependencyManager.shared.tezosNodeClient.estimate(operations: operations, withWallet: wallet) { result in
-			switch result {
-				case .success(let ops):
+		
+		if isXtzToToken, let xtz = xtzToSwap {
+			
+			let operations = OperationFactory.liquidityBakingXtzToToken(xtzAmount: xtz, minTokenAmount: calc.minimum, contract: "KT1TxqZ8QtKvLu3V3JH7Gx58n7Co8pgtpQU5", wallet: wallet, timeout: 60 * 5)
+			DependencyManager.shared.tezosNodeClient.estimate(operations: operations, withWallet: wallet) { result in
+				switch result {
+					case .success(let ops):
+						
+						DependencyManager.shared.tezosNodeClient.send(operations: ops, withWallet: wallet) { innerResult in
+							switch innerResult {
+								case .success(let opHash):
+									self.alert(withTitle: "Success", andMessage: "Op hash: \(opHash)")
+									
+								case .failure(let error):
+									self.alert(withTitle: "Error", andMessage: error.description)
+							}
+						}
 					
-					DependencyManager.shared.tezosNodeClient.send(operations: ops, withWallet: wallet) { innerResult in
-						print("Inner Result: \(innerResult)")
-					}
-				
-				case .failure(let _):
-					print("fail")
+					case .failure(let error):
+						self.alert(withTitle: "Error", andMessage: error.description)
+				}
+			}
+			
+		} else if let token = tokenToSwap {
+			let operations = OperationFactory.liquidityBakingTokenToXTZ(
+				tokenAmount: token,
+				minXTZAmount: calc.minimum as? XTZAmount ?? XTZAmount.zero(),
+				contract: "KT1TxqZ8QtKvLu3V3JH7Gx58n7Co8pgtpQU5",
+				tokenContract: "KT1VqarPDicMFn1ejmQqqshUkUXTCTXwmkCN",
+				currentAllowance: TokenAmount(fromNormalisedAmount: 1, decimalPlaces: 0),
+				wallet: wallet,
+				timeout: 60 * 5
+			)
+			
+			DependencyManager.shared.tezosNodeClient.estimate(operations: operations, withWallet: wallet) { result in
+				switch result {
+					case .success(let ops):
+						
+						DependencyManager.shared.tezosNodeClient.send(operations: ops, withWallet: wallet) { innerResult in
+							switch innerResult {
+								case .success(let opHash):
+									self.alert(withTitle: "Success", andMessage: "Op hash: \(opHash)")
+									
+								case .failure(let error):
+									self.alert(withTitle: "Error", andMessage: error.description)
+							}
+						}
+					
+					case .failure(let error):
+						self.alert(withTitle: "Error", andMessage: error.description)
+				}
 			}
 		}
 	}
@@ -63,13 +106,23 @@ class ExchangeLiquidityBakingViewController: UIViewController {
 			return
 		}
 		
-		guard let input = inputTextField.text, let xtz = XTZAmount(fromNormalisedAmount: input, decimalPlaces: 6) else {
-			self.alert(withTitle: "Error", andMessage: "Invalid amount of XTZ")
-			return
+		if isXtzToToken {
+			guard let input = inputTextField.text, let xtz = XTZAmount(fromNormalisedAmount: input, decimalPlaces: 6) else {
+				self.alert(withTitle: "Error", andMessage: "Invalid amount of XTZ")
+				return
+			}
+			
+			self.calculationResult = DexterCalculationService.shared.calculateXtzToToken(xtzToSell: xtz, dexterXtzPool: pData.xtzPool, dexterTokenPool: pData.tokenPool, maxSlippage: 0.5)
+			
+		} else {
+			guard let input = inputTextField.text, let token = TokenAmount(fromNormalisedAmount: input, decimalPlaces: 8) else {
+				self.alert(withTitle: "Error", andMessage: "Invalid amount of XTZ")
+				return
+			}
+			
+			self.calculationResult = DexterCalculationService.shared.calcualteTokenToXTZ(tokenToSell: token, dexterXtzPool: pData.xtzPool, dexterTokenPool:  pData.tokenPool, maxSlippage: 0.5)
 		}
 		
-		self.xtz = xtz
-		self.calculationResult = DexterCalculationService.shared.calculateXtzToToken(xtzToSell: xtz, dexterXtzPool: pData.xtzPool, dexterTokenPool: pData.tokenPool, maxSlippage: 0.5)
 		
 		guard let calc = self.calculationResult else {
 			outputTextField.text = "0"
