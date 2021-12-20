a5ddbaf9c5bcff2ec853ea99deb6acb86114f4f6
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-12-20 17:32:36 +0000

    - bump iOS deployment to iOS 15 to make use of bottom sheet - add some images and colour codes from figma - initial welcome and login modal

---
+++ Kukai Mobile/Modules/Onboarding/GetStartedViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GetStartedViewController.swift
+//  Kukai Mobile
+//
+//  Created by Simon Mcloughlin on 20/12/2021.
+//
+
+import UIKit
+
+class GetStartedViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Kukai Mobile/Modules/Onboarding/WelcomeViewController.swift
+++ Kukai Mobile/Modules/Onboarding/WelcomeViewController.swift
@@ -18,10 +18,25 @@ class WelcomeViewController: UIViewController {
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		
+		/*
 		self.navigationController?.setNavigationBarHidden(false, animated: false)
 		self.navigationItem.hidesBackButton = true
 		
 		let buttonText = DependencyManager.shared.currentNetworkType == .mainnet ? "Mainnet" : "Testnet"
 		networkButton.setTitle(buttonText, for: .normal)
+		*/
+	}
+	
+	@IBAction func getStartedTapped(_ sender: Any) {
+		let storyboard = UIStoryboard(name: "Onboarding", bundle: nil)
+		let viewController = storyboard.instantiateViewController(withIdentifier: "GetStartedBottomSheet")
+		
+		if let presentationController = viewController.presentationController as? UISheetPresentationController {
+			presentationController.detents = [.medium()]
+			presentationController.prefersGrabberVisible = true
+			presentationController.preferredCornerRadius = 16
+		}
+		
+		self.present(viewController, animated: true)
 	}
 }
