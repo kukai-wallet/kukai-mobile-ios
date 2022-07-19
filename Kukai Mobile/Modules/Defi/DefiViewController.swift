aa64190c889cea636498130c735b7cbb652ea8fd
Simon McLoughlin <simon.d.mcl@gmail.com>
2022-07-19 11:20:35 +0100

    - create new defi storyboard / folder - move old defi screens over to new storyboard until replaced - skeleton initial view controller

---
+++ Kukai Mobile/Modules/Defi/DefiViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DefiViewController.swift
+//  Kukai Mobile
+//
+//  Created by Simon Mcloughlin on 19/07/2022.
+//
+
+import UIKit
+
+class DefiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
