92ccc5e3aeef67f54000514df06216655692481c
Simon McLoughlin <simon.d.mcl@gmail.com>
2022-07-29 14:57:59 +0100

    - added skelton for add / remove liquidity - removed old swap liquidity code

--- Kukai Mobile/Modules/Defi/LiquidityViewController.swift
+++ Kukai Mobile/Modules/Defi/LiquidityViewController.swift
@@ -8,8 +8,35 @@
 import UIKit
 
 class LiquidityViewController: UIViewController {
-
+	
+	@IBOutlet weak var addContainer: UIView!
+	@IBOutlet weak var removeContainer: UIView!
+	@IBOutlet weak var settingsButton: UIButton!
+	@IBOutlet weak var segmentedButton: UISegmentedControl!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
+		
+		showAdd()
     }
+	
+	@IBAction func segmentedButtonChanged(_ sender: Any) {
+		if segmentedButton.selectedSegmentIndex == 0 {
+			showAdd()
+		} else {
+			showRemove()
+		}
+	}
+	
+	func showAdd() {
+		addContainer.isHidden = false
+		removeContainer.isHidden = true
+		settingsButton.isHidden = false
+	}
+	
+	func showRemove() {
+		addContainer.isHidden = true
+		removeContainer.isHidden = false
+		settingsButton.isHidden = true
+	}
 }
