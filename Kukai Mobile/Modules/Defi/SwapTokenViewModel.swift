6ba5372ea89367ca5f2aa19579b1e7d304254a94
Simon McLoughlin <simon.d.mcl@gmail.com>
2023-02-10 15:27:32 +0000

    fix compiler issues

--- Kukai Mobile/Modules/Defi/SwapTokenViewModel.swift
+++ Kukai Mobile/Modules/Defi/SwapTokenViewModel.swift
@@ -59,11 +59,7 @@ class SwapTokenViewModel: ViewModel, UITableViewDiffableDataSourceHandler {
 	}
 	
 	func refresh(animate: Bool, successMessage: String? = nil) {
-		guard let address = DependencyManager.shared.selectedWalletAddress else {
-			self.state = .failure(KukaiError.internalApplicationError(error: ViewModelError.dataSourceNotCreated), "Unable to process data at this time")
-			return
-		}
-		
+		let address = DependencyManager.shared.selectedWalletAddress
 		if DependencyManager.shared.balanceService.exchangeData.count == 0 {
 			DependencyManager.shared.balanceService.fetchAllBalancesTokensAndPrices(forAddress: address, refreshType: .useCache) { [weak self] error in
 				if let err = error {
