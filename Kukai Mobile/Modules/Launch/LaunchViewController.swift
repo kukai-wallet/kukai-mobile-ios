74ac2f8814654380d2e6e7e9c34fecda6b3a3817
Simon McLoughlin <simon.d.mcl@gmail.com>
2023-03-31 17:33:22 +0100

    - remove old "prototype" related files and storyboard scenes - WIP: re-style launch, main and onboarding home - add basic animation to transitions

--- Kukai Mobile/Modules/Launch/LaunchViewController.swift
+++ Kukai Mobile/Modules/Launch/LaunchViewController.swift
@@ -12,6 +12,74 @@ import os.log
 class LaunchViewController: UIViewController, CAAnimationDelegate {
 	
 	@IBOutlet weak var kukaiLogo: UIImageView!
+	@IBOutlet weak var logoWidthConstraint: NSLayoutConstraint!
+	@IBOutlet weak var logoHeightConstraint: NSLayoutConstraint!
+	@IBOutlet var logoCenterConstraint: NSLayoutConstraint!
+	@IBOutlet var logoTopConstraint: NSLayoutConstraint!
+	
+	@IBOutlet weak var logoText: UILabel!
+	
+	private var runOnce = false
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let _ = self.view.addGradientBackgroundFull()
+		
+		if !runOnce {
+			logoTopConstraint.isActive = false
+			logoCenterConstraint.isActive = true
+			
+		} else {
+			logoTopConstraint.isActive = true
+			logoCenterConstraint.isActive = false
+			
+			logoWidthConstraint.constant = 200
+			logoHeightConstraint.constant = 55
+		}
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		if !runOnce {
+			animate()
+			
+		} else {
+			disolveTransition()
+		}
+	}
+	
+	private func animate() {
+		logoWidthConstraint.constant = 200
+		logoHeightConstraint.constant = 55
+		logoTopConstraint.isActive = true
+		logoCenterConstraint.isActive = false
+		
+		UIView.animate(withDuration: 0.6, delay: 0.3) {
+			self.view.layoutIfNeeded()
+			
+		} completion: { [weak self] success in
+			self?.disolveTransition()
+		}
+	}
+	
+	private func disolveTransition() {
+		self.navigationItem.hidesBackButton = true
+		self.navigationItem.largeTitleDisplayMode = .never
+		
+		runOnce = true
+		if DependencyManager.shared.walletList.count > 0 {
+			self.performSegue(withIdentifier: "home", sender: nil)
+		} else {
+			self.performSegue(withIdentifier: "onboarding", sender: nil)
+		}
+	}
+	
+	
+	
+	/*
+	@IBOutlet weak var kukaiLogo: UIImageView!
 	@IBOutlet weak var safeImage: UIImageView!
 	@IBOutlet weak var kukaiTextImage: UIImageView!
 	@IBOutlet weak var kukaiTextCoverView: UIView!
@@ -352,4 +420,5 @@ class LaunchViewController: UIViewController, CAAnimationDelegate {
 			self.performSegue(withIdentifier: "onboarding", sender: nil)
 		}
 	}
+	 */
 }
--- Kukai Mobile/Modules/Onboarding/WelcomeViewController.swift
+++ Kukai Mobile/Modules/Onboarding/WelcomeViewController.swift
@@ -9,10 +9,9 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 	
-	@IBOutlet weak var networkButton: UIButton!
-	
 	override func viewDidLoad() {
         super.viewDidLoad()
+		let _ = self.view.addGradientBackgroundFull()
     }
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -24,4 +23,49 @@ class WelcomeViewController: UIViewController {
 		
 		DependencyManager.shared.setDefaultMainnetURLs()
 	}
+	
+	
+	/*
+	 feeButton.configuration?.imagePlacement = .trailing
+	 feeButton.configuration?.imagePadding = 6
+	 feeButton.isEnabled = false
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	private var gradient = CAGradientLayer()
+	
+	func setup(data: TokenDetailsSendData) {
+		
+		if data.isBuyTez {
+			var image = UIImage(named: "Plus")
+			image = image?.resizedImage(size: CGSize(width: 15, height: 15))
+			image = image?.withTintColor(.colorNamed("TxtBtnPrim1"))
+			
+			sendButton.setImage(image, for: .normal)
+			sendButton.configuration?.attributedTitle = AttributedString("Get Tez", attributes: AttributeContainer( [NSAttributedString.Key.font: UIFont.custom(ofType: .bold, andSize: 20)] ))
+			sendButton.configuration?.imagePadding = 8
+			sendButton.configuration?.imagePlacement = .leading
+			
+		} else {
+			sendButton.configuration?.attributedTitle = AttributedString("Send", attributes: AttributeContainer( [NSAttributedString.Key.font: UIFont.custom(ofType: .bold, andSize: 20)] ))
+			sendButton.configuration?.imagePadding = 8
+			sendButton.configuration?.imagePlacement = .trailing
+		}
+	}
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		gradient.removeFromSuperlayer()
+		gradient = sendButton.addGradientButtonPrimary(withFrame: sendButton.bounds)
+	}
+	*/
+	
 }
