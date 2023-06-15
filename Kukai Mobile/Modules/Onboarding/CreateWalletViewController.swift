655d63ab59463370dc23ae6d4aadd2370c645fa1
Simon McLoughlin <simon.d.mcl@gmail.com>
2023-06-15 17:26:11 +0100

    remove learn more button code

--- Kukai Mobile/Modules/Onboarding/CreateWalletViewController.swift
+++ Kukai Mobile/Modules/Onboarding/CreateWalletViewController.swift
@@ -11,21 +11,14 @@ import KukaiCoreSwift
 class CreateWalletViewController: UIViewController {
 	
 	@IBOutlet var socialWalletButton: CustomisableButton!
-	@IBOutlet var socialLearnMoreButton: CustomisableButton!
 	@IBOutlet var hdWalletButton: CustomisableButton!
-	@IBOutlet var hdLearnMoreButton: CustomisableButton!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		let _ = self.view.addGradientBackgroundFull()
 		
 		socialWalletButton.customButtonType = .primary
-		socialLearnMoreButton.configuration?.imagePlacement = .trailing
-		socialLearnMoreButton.configuration?.imagePadding = 8
-		
 		hdWalletButton.customButtonType = .tertiary
-		hdLearnMoreButton.configuration?.imagePlacement = .trailing
-		hdLearnMoreButton.configuration?.imagePadding = 8
     }
 	
 	@IBAction func hdWalletTapped(_ sender: Any) {
