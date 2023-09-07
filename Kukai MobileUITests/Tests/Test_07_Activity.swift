6b28b3e008205a2cbc3c335164b8bfd52fb481b7
Simon McLoughlin <simon.d.mcl@gmail.com>
2023-09-07 15:09:46 +0100

    - add some tests for activity - add some tests for discover, including switching to mainnet to see real values

--- Kukai MobileUITests/Tests/Test_07_Activity.swift
+++ Kukai MobileUITests/Tests/Test_07_Activity.swift
@@ -5,10 +5,93 @@
 //  Created by Simon Mcloughlin on 06/09/2023.
 //
 
-/*
 import XCTest
 
 final class Test_07_Activity: XCTestCase {
 	
+	
+	// MARK: - Setup
+	
+	override func setUpWithError() throws {
+		continueAfterFailure = false
+		
+		SharedHelpers.shared.application().launch()
+	}
+	
+	override func tearDownWithError() throws {
+		
+	}
+	
+	
+	
+	// MARK: - Test functions
+	
+	// Accounts are public, possible airdrop tests etc can show up. We know we have done many things up to this point, we have sent XTZ, a token, collectible and changed bakers twice.
+	// Verify all those things are present in the list
+	func testVerifyItemsPresent() {
+		let app = XCUIApplication()
+		Test_03_Home.handleLoginIfNeeded(app: app)
+		Test_03_Home.handleOpenActivityTab(app: app)
+		sleep(2)
+		
+		let normalCells = app.tables.cells.containing(.staticText, identifier: "activity-item-title")
+		
+		
+		// Check for XTZ + kUSD sends
+		let tokenSends = normalCells.containing(.staticText, identifier: "Send")
+		var foundXTZSend = false
+		var foundkUSDSend = false
+		
+		for i in 0..<tokenSends.count {
+			let title = tokenSends.element(boundBy: i).staticTexts["activity-item-title"].label
+			let components = title.components(separatedBy: " ")
+			
+			if components.count > 1 {
+				if components[1] == "XTZ" {
+					foundXTZSend = true
+					
+				} else if components[1] == "kUSD" {
+					foundkUSDSend = true
+				}
+			}
+			
+			if foundXTZSend && foundkUSDSend {
+				break
+			}
+		}
+		XCTAssert(foundXTZSend && foundkUSDSend)
+		
+		
+		
+		// Check for kUSD receives
+		let tokenReceives = normalCells.containing(.staticText, identifier: "Receive")
+		var foundkUSDReceive = false
+		
+		for i in 0..<tokenReceives.count {
+			let title = tokenReceives.element(boundBy: i).staticTexts["activity-item-title"].label
+			let components = title.components(separatedBy: " ")
+			
+			if components.count > 1 {
+				if components[1] == "kUSD" {
+					foundkUSDReceive = true
+				}
+			}
+			
+			if foundkUSDReceive {
+				break
+			}
+		}
+		XCTAssert(foundkUSDReceive)
+		
+		
+		// Check for NFTs
+		let nftTransfers = normalCells.containing(.staticText, identifier: "Tasty Cookie")
+		XCTAssert(nftTransfers.staticTexts["Receive"].exists)
+		XCTAssert(nftTransfers.staticTexts["Send"].exists)
+		
+		
+		// Check for delegations
+		XCTAssert(normalCells.staticTexts["Delegate"].exists)
+		XCTAssert(normalCells.staticTexts[" Baking Benjamins"].exists)
+	}
 }
-*/
--- Kukai MobileUITests/Tests/Test_08_Discover.swift
+++ Kukai MobileUITests/Tests/Test_08_Discover.swift
@@ -5,10 +5,74 @@
 //  Created by Simon Mcloughlin on 06/09/2023.
 //
 
-/*
 import XCTest
 
 final class Test_08_Discover: XCTestCase {
 	
+	
+	// MARK: - Setup
+	
+	override func setUpWithError() throws {
+		continueAfterFailure = false
+		
+		SharedHelpers.shared.application().launch()
+	}
+	
+	override func tearDownWithError() throws {
+		
+	}
+	
+	// "discover-item-image"
+	// titleLabel.accessibilityIdentifier = "discover-item-show-more-title"
+	// collectionView.accessibilityIdentifier = "discover-featured-cell"
+	// "discover-featured-cell-image"
+	
+	
+	
+	// MARK: - Test functions
+	
+	func testDiscover() {
+		let app = XCUIApplication()
+		Test_03_Home.handleLoginIfNeeded(app: app)
+		Test_03_Home.handleOpenDiscoverTab(app: app)
+		sleep(2)
+		
+		
+		// Basic checks of ghostnet data
+		let ghostnetItemCount = app.tables.cells.containing(.image, identifier: "discover-item-image").count
+		XCTAssert(ghostnetItemCount > 0)
+		
+		
+		// Switch to mainnet + check for more advanced content
+		Test_03_Home.handleOpenSideMenu(app: app)
+		sleep(2)
+		
+		app.tables.staticTexts["Network"].tap()
+		sleep(2)
+		
+		app.tables.staticTexts["Mainnet"].tap()
+		sleep(4)
+		
+		app.buttons["side-menu-close-button"].tap()
+		sleep(2)
+		
+		
+		let mainnetItemCount = app.tables.cells.containing(.image, identifier: "discover-item-image").count
+		XCTAssert(mainnetItemCount > ghostnetItemCount)
+		XCTAssert(app.tables.cells.containing(.pageIndicator, identifier: "discover-featured-page-control").count > 0)
+		
+		
+		// Switch back to ghostnet
+		Test_03_Home.handleOpenSideMenu(app: app)
+		sleep(2)
+		
+		app.tables.staticTexts["Network"].tap()
+		sleep(2)
+		
+		app.tables.staticTexts["Ghostnet"].tap()
+		sleep(4)
+		
+		app.buttons["side-menu-close-button"].tap()
+		sleep(2)
+	}
 }
-*/
