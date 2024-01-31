592a58a25d79ceab7424045aba251e97c894b046
Simon McLoughlin <simon.d.mcl@gmail.com>
2024-01-31 13:55:51 +0000

    WIP: batch
    - display each operation as a tableview cell

--- Kukai Mobile/Modules/Send/SendBatchDetailsViewController.swift
+++ Kukai Mobile/Modules/Send/SendBatchDetailsViewController.swift
@@ -11,6 +11,7 @@ import KukaiCoreSwift
 class SendBatchDetailsViewController: UIViewController {
 	
 	@IBOutlet weak var headerLabel: UILabel!
+	@IBOutlet weak var tableView: UITableView!
 	
 	@IBOutlet weak var descriptionStackViews: UIStackView!
 	@IBOutlet weak var transferAmountStackView: UIStackView!
@@ -24,14 +25,19 @@ class SendBatchDetailsViewController: UIViewController {
 	
 	private var batchData: TransactionService.BatchData? = nil
 	private var currentSummary: TransactionService.BatchOpSummary? = nil
+	private var selectedIndex = 0
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
 		
 		batchData = isWalletConnectOp() ? TransactionService.shared.walletConnectOperationData.batchData : TransactionService.shared.batchData
 		currentSummary = batchData?.opSummaries?.first
+		selectedIndex = batchData?.selectedOp ?? 0
 		
 		updateDisplay()
+		
+		tableView.dataSource = self
+		tableView.delegate = self
     }
 	
 	private func updateDisplay() {
@@ -71,16 +77,15 @@ class SendBatchDetailsViewController: UIViewController {
 	}
 	
 	private func addJSONToTextView() {
-		let index = batchData?.selectedOp ?? 0
 		let ops = selectedOperationsAndFees()
-		guard ops.count > index else {
+		guard ops.count > selectedIndex else {
 			operationTextView.text = ""
 			return
 		}
 		
-		let op = ops[index]
+		let op = ops[selectedIndex]
 		let encoder = JSONEncoder()
-		encoder.outputFormatting = .prettyPrinted
+		encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
 		
 		let data = (try? encoder.encode(op)) ?? Data()
 		let string = String(data: data, encoding: .utf8)
@@ -90,5 +95,49 @@ class SendBatchDetailsViewController: UIViewController {
 	@IBAction func operationCopyButton(_ sender: UIButton) {
 		Toast.shared.show(withMessage: "copied!", attachedTo: sender)
 		UIPasteboard.general.string = operationTextView.text
+	}
+}
+
+extension SendBatchDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return batchData?.opSummaries?.count ?? 0
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: "BatchDetailOperationCell", for: indexPath) as? BatchDetailOperationCell else {
+			return UITableViewCell()
+		}
+		
+		cell.addressLabel.text = batchData?.opSummaries?[indexPath.row].contractAddress?.truncateTezosAddress() ?? ""
+		return cell
+	}
+	
+	func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+		if indexPath.row == selectedIndex {
+			cell.setSelected(true, animated: true)
+			
+		} else {
+			cell.setSelected(false, animated: true)
+		}
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.cellForRow(at: IndexPath(row: selectedIndex, section: indexPath.section))?.setSelected(false, animated: true)
+		tableView.cellForRow(at: indexPath)?.setSelected(true, animated: true)
+		
+		selectedIndex = indexPath.row
+		currentSummary = batchData?.opSummaries?[selectedIndex]
+		if isWalletConnectOp() {
+			TransactionService.shared.walletConnectOperationData.batchData.selectedOp = indexPath.row
+		} else {
+			TransactionService.shared.batchData.selectedOp = indexPath.row
+		}
+		
+		updateDisplay()
 	}
 }
