42d8e053d95b47b637a3bf2eb94db16dd59be45d
Simon McLoughlin <simon.d.mcl@gmail.com>
2024-07-12 12:27:02 +0100

    - add layout for "looking for devices" screen - added extra option to already have screen - wired up options to link to new ledger screen

---
+++ Kukai Mobile/Modules/Onboarding/LookingForDevicesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LookingForDevicesViewController.swift
+//  Kukai Mobile
+//
+//  Created by Simon Mcloughlin on 12/07/2024.
+//
+
+import UIKit
+
+class LookingForDevicesViewController: UIViewController {
+
+	@IBOutlet weak var spinnerImage: UIImageView!
+	@IBOutlet weak var tableView: UITableView!
+	@IBOutlet weak var pairButton: CustomisableButton!
+	@IBOutlet weak var palceholderView: UIView!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
